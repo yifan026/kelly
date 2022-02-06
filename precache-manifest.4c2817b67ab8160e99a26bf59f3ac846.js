6f2351a8470d6562487f7f31ba622a5eb893ab9d
yifan <yif.y.chen@gmail.com>
2022-02-06 18:50:52 +0800

    update

---
+++ precache-manifest.4c2817b67ab8160e99a26bf59f3ac846.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a98420a998fc7175be3ad39327455512",
+    "url": "/kelly/index.html"
+  },
+  {
+    "revision": "6dc19556bbed59fadda2",
+    "url": "/kelly/static/css/main.6d690e8b.chunk.css"
+  },
+  {
+    "revision": "437e09bdf7a2c6fd2ad1",
+    "url": "/kelly/static/js/2.b0f03853.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/kelly/static/js/2.b0f03853.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6dc19556bbed59fadda2",
+    "url": "/kelly/static/js/main.ab2e3b0b.chunk.js"
+  },
+  {
+    "revision": "94b5202811b7a80519bd",
+    "url": "/kelly/static/js/runtime-main.0dd38709.js"
+  },
+  {
+    "revision": "013f0b90a4cae7b8bbf13e3fd9e7dc4a",
+    "url": "/kelly/static/media/getFetch.013f0b90.cjs"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/kelly/static/media/logo.ee7cd8ed.svg"
+  }
+]);
